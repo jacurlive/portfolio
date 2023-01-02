ed73ec1600d3a730e27071a82fe24d1967f8a891
kkirchh <jasurkkirchh@gmail.com>
2023-01-03 00:57:53 +0500

    add model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from app.models import About
+
+
+@admin.register(About)
+class AboutAdmin(ModelAdmin):
+    fields = ('image', 'phone', 'city', 'age', 'degree', 'email', 'description')
+    list_display = ('id', 'full_name')
--- app/models.py
+++ app/models.py
@@ -1,6 +1,20 @@
-from django.db import models
-from django.db.models import Model, ImageField
+from django.db.models import Model, ImageField, CharField, IntegerField, BooleanField, TextField, URLField
 
 
-class Projects(Model):
-    image = ImageField
+class About(Model):
+    image = ImageField(upload_to='about/')
+    full_name = CharField(max_length=300, default='Jasur Juraev')
+    birthday = CharField(max_length=200, default='26 January 2005')
+    web_site = CharField(max_length=155, default='www.jasur.com')
+    phone = CharField(max_length=200, default='+998 (94) 800 2005')
+    city = CharField(max_length=200, default='Tashkent, UZB')
+    age = IntegerField(default=17)
+    degree = CharField(max_length=100, default='Junior')
+    email = CharField(max_length=300, default='jasurkkirchh@gmail.com')
+    freelance = BooleanField(default=True)
+    description = TextField()
+    instagram_link = URLField(null=True)
+    facebook_link = URLField(null=True)
+    twitter_link = URLField(null=True)
+    linkedin_link = URLField(null=True)
+
